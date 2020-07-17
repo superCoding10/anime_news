41f1c301d22f038369568785fd1e8dd1718377c6
aihara <aihara@aihara-desktop.zte.com.cn>
2020-07-17 12:41:49 +0300

    commit all

--- index.php
+++ index.php
@@ -17,42 +17,36 @@
     
 ?>
     <div class="wrapper">
+
         <div class="welcome_block">
             <h1>THE MOST ACTUAL ANIME AND MANGA NEWS!</h1>
             <p>Here you will find everything you want to know about anime culture, anime art, anime adaptation and more</p>
             <button type="button" class="btn btn-outline-info read_news_btn">Read news</button>
         </div>
 
-<div class="search_news_wrapper" style="margin: 132px 0 0 25%;
-display: flex;">
-<input type="text" placeholder="Search news..." style="width: 36%;
-background: transparent;
-border-radius: 8px;
-border-color: #17a2b8;
-height: 50px;
-padding-left: 12px;
-color: #f1f2f2;
-font-size: 20px;
-margin-right: 17px;
-padding-right: 10px;">
-<button style="font-size: 24px;" class="btn btn-outline-info">Search</button>
-</div>
-
-<div style="position: absolute; top: 170px; right: 6%;" class="random_news_card">  
-<h3 style="color: #53e8e8;margin-bottom: 17px;">Random news</h3>  
-    <div class="card second_card" style="
-            background: #26252561;
-            width: 283px;
-            color: #fbfbfb;
-            border: 1px solid white;
-            padding: 0px;">
-        <img src="/user_images/current_img.jpg" alt="" style="width: 100%;">
-        <div>
-        <p style="font-size: 24px; text-align: center; margin-top: 22px;">asd asd sadsad asd sad sadsad sad sa d</p>
-        <p style="margin: 12px; margin-top: 12px; font-size: 19px; margin-top: 0;">asd asd sadsad asd sad sadsad sad sa dasd asd sad asd sad asd sad asd asd ...</p>
+        <div class="search_news_wrapper">
+            <input type="text" placeholder="Search news...">
+            <button class="btn btn-outline-info">Search</button>
         </div>
-        <button type="button" class="btn btn-outline-info read_news_btn" style="margin: 5px">Read</button>
-    </div>
-</div>
+
+        <div class="random_news_card">  
+        <h3>Random news</h3>  
+            <div class="card">
+                <!-- CURR IMG -->
+                <img id="card_curr_img" src="/user_images/current_img.jpg" alt="random_news_img" style="height: 175px;transition: all 1s;">
+                <!-- NEW IMG -->
+                <img id="card_new_img" src="/user_images/1.jpg" style="position: absolute;height: 175px;transition: all 1s;opacity: 0;" alt="random_news_img">
+
+                <div class="card_text_content">
+                <!-- CURR HEADER -->
+                    <p style="transition: all 1s;" id="card_curr_header"  class="card_header text-center">asd asd sadsad asd sad sadsad sad sa d</p>
+                <!-- NEW HEADER -->
+                    <p id="card_new_header" style="position: absolute;opacity: 0;padding: 0 7px; width: 284px;top: 175px;transition: all 1s;" class="card_header text-center">Using this kind of material will lead you to...</p>
+
+                    <p class="card_body">asd asd sadsad asd sad sadsad sad sa dasd asd sad asd sad asd sad asd asd ...</p>
+                </div>
+                <button type="button" class="btn btn-outline-info read_news_btn">Read</button>
+            </div>
+        </div>
 
 </div>
---
+++ js/index.js
@@ -0,0 +1,42 @@
+let first = "first first first first first"
+let second = "second second second second second"
+let third = "third third third third"
+let forth = "forth forth forth forth"
+let arr = [first, second, third, forth]
+let index = 0
+let x = true
+
+
+// let intrvl =  setInterval(() => {
+// if(index === arr.length) {
+//         alert("end")
+//         clearInterval(intrvl)
+//     }
+//     // img.style.opacity = "1"
+//     card_new_header.textContent = arr[index++]
+//     card_curr_header.style.opacity = 0
+//     card_new_header.style.opacity = 1
+
+//     card_new_img.src = "/user_images/" + index + ".jpg"
+//     card_curr_img.style.opacity = 0
+//     card_new_img.style.opacity = 1
+
+//     if(x) {
+//         card_new_header.id = "card_curr_header"
+//         card_curr_header.id = "card_new_header"
+
+//         card_new_img.id = "card_curr_img"
+//         card_curr_img.id = "card_new_img"
+
+//     } else {
+
+
+//         card_curr_header.id = "card_new_header"
+//         card_new_header.id = "card_curr_header"
+
+//         card_curr_img.id = "card_new_img"
+//         card_new_img.id = "card_curr_img"
+//     }
+//     x = !x
+    
+// }, 3000)
