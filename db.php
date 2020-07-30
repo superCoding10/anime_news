c6aed663a32a744d12c8d0ccb2b0a6e0323aef71
aihara <petr_konyuk@mail.ru>
2020-07-30 10:45:37 +0300

    commit db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+echo "db";
+$dsn = 'mysql:dbname=sql2357615;host=sql2.freemysqlhosting.net';
+$user = "sql2357615";
+$password = "aT3*xY8!";
+try {
+    //code...
+    $mysqli = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    //throw $th;
+    echo "failed: ";
+    echo $e->getMessage();
+}
